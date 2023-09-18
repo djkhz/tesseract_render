68852689ffa3e3d8aa3277f4f2afbecca524b239
djkhz <zheezs@hotmail.com>
2023-09-18 12:01:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,5 +14,5 @@ def hello_world():
 @app.route('/ocr', methods=['GET','POST'])
 def ocr():
     # test.png from the pytesseract project: https://github.com/madmaze/pytesseract/tree/master/tests/data
-    return pytesseract.get_tesseract_version 
+    return pytesseract.get_tesseract_version()
     #return pytesseract.image_to_string(Image.open('test.png'))
