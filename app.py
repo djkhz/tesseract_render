f30914d3772663e5dad2e102f3f7600ef552fc07
djkhz <zheezs@hotmail.com>
2023-09-18 11:44:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,14 @@ import pytesseract
 
 app = Flask(__name__)
 
+
 @app.route('/')
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+    
+@app.route('/ocr', methods=['GET','POST'])
 def ocr():
     # test.png from the pytesseract project: https://github.com/madmaze/pytesseract/tree/master/tests/data
     return pytesseract.image_to_string(Image.open('test.png'))
