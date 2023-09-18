1f9f3de1fbc57a79486d6b169bc7766abd0be121
djkhz <zheezs@hotmail.com>
2023-09-18 22:03:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -59,52 +59,52 @@ Imnp =np.array([])
 def hello_world():
     return 'Hello, World!'
     
-@app.route('/ocr', methods=['GET','POST'])
-def ocr():
-    # test.png from the pytesseract project: https://github.com/madmaze/pytesseract/tree/master/tests/data
-    #return pytesseract.get_tesseract_version()
-    return pytesseract.image_to_string(Image.open('test.png'), lang='eng')
+# @app.route('/ocr', methods=['GET','POST'])
+# def ocr():
+#     # test.png from the pytesseract project: https://github.com/madmaze/pytesseract/tree/master/tests/data
+#     #return pytesseract.get_tesseract_version()
+#     return pytesseract.image_to_string(Image.open('test.png'), lang='eng')
 
-@app.route('/ocx', methods=['GET','POST'])
-def ocx():
-    # test.png from the pytesseract project: https://github.com/madmaze/pytesseract/tree/master/tests/data
-    #return pytesseract.get_tesseract_version()
-    pytesseract.pytesseract.tesseract_cmd = '/app/.usr/bin/tesseract'
-    return pytesseract.image_to_string(Image.open('test.png'), lang='eng')
+# @app.route('/ocx', methods=['GET','POST'])
+# def ocx():
+#     # test.png from the pytesseract project: https://github.com/madmaze/pytesseract/tree/master/tests/data
+#     #return pytesseract.get_tesseract_version()
+#     pytesseract.pytesseract.tesseract_cmd = '/app/.usr/bin/tesseract'
+#     return pytesseract.image_to_string(Image.open('test.png'), lang='eng')
 
-@app.route('/ocy', methods=['GET','POST'])
-def ocy():
-    # test.png from the pytesseract project: https://github.com/madmaze/pytesseract/tree/master/tests/data
-    #return pytesseract.get_tesseract_version()
-    pytesseract.pytesseract.tesseract_cmd = '/app/usr/bin/tesseract'
-    return pytesseract.image_to_string(Image.open('test.png'), lang='eng')
+# @app.route('/ocy', methods=['GET','POST'])
+# def ocy():
+#     # test.png from the pytesseract project: https://github.com/madmaze/pytesseract/tree/master/tests/data
+#     #return pytesseract.get_tesseract_version()
+#     pytesseract.pytesseract.tesseract_cmd = '/app/usr/bin/tesseract'
+#     return pytesseract.image_to_string(Image.open('test.png'), lang='eng')
 
-@app.route('/ocz', methods=['GET','POST'])
-def ocz():
-    # test.png from the pytesseract project: https://github.com/madmaze/pytesseract/tree/master/tests/data
-    #return pytesseract.get_tesseract_version()
-    pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
-    return pytesseract.image_to_string(Image.open('test.png'), lang='eng')
+# @app.route('/ocz', methods=['GET','POST'])
+# def ocz():
+#     # test.png from the pytesseract project: https://github.com/madmaze/pytesseract/tree/master/tests/data
+#     #return pytesseract.get_tesseract_version()
+#     pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
+#     return pytesseract.image_to_string(Image.open('test.png'), lang='eng')
        
-@app.errorhandler(404)
-def not_found(error):
-  resp = jsonify( { 
-    u'status': 404, 
-    u'message': u'Resource not found' 
-  } )
-  resp.status_code = 404
-  return resp
+# @app.errorhandler(404)
+# def not_found(error):
+#   resp = jsonify( { 
+#     u'status': 404, 
+#     u'message': u'Resource not found' 
+#   } )
+#   resp.status_code = 404
+#   return resp
     
-def process_file(path, filename):
+# def process_file(path, filename):
     
-    # json_data = {path: filename}
-    # return json_data
-    file_name = path.replace(filename,secure_filename(filename))
-    # img3 = cv2.imread(file_name)
-    # img3 = np.array(Imageocr)
-    gray3 = cv2.cvtColor(Imnp, cv2.COLOR_BGR2GRAY) 
+#     # json_data = {path: filename}
+#     # return json_data
+#     file_name = path.replace(filename,secure_filename(filename))
+#     # img3 = cv2.imread(file_name)
+#     # img3 = np.array(Imageocr)
+#     gray3 = cv2.cvtColor(Imnp, cv2.COLOR_BGR2GRAY) 
 
-    text3 = pytesseract.image_to_string(gray3, lang='lao', config='--psm 11')
-    # target = pytesseract.image_to_string(image, lang='eng', boxes=False, \
-        # config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789')
-    return text3
+#     text3 = pytesseract.image_to_string(gray3, lang='lao', config='--psm 11')
+#     # target = pytesseract.image_to_string(image, lang='eng', boxes=False, \
+#         # config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789')
+#     return text3
