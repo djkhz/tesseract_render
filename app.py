41ace77c4bf891b7169402d36dc16e35a02e7028
djkhz <zheezs@hotmail.com>
2023-09-18 19:48:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -67,11 +67,23 @@ def ocr():
     
 @app.route('/ocx', methods=['GET','POST'])
 def ocx():
-    pytesseract.pytesseract.tesseract_cmd = '/app/.usr/bin/tesseract'
+    pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
     # test.png from the pytesseract project: https://github.com/madmaze/pytesseract/tree/master/tests/data
     #return pytesseract.get_tesseract_version()
     return pytesseract.image_to_string(Image.open('test.png'), lang='eng')
     
+ def ocxy
+    pytesseract.pytesseract.tesseract_cmd = '/app/usr/bin/tesseract'
+    # test.png from the pytesseract project: https://github.com/madmaze/pytesseract/tree/master/tests/data
+    #return pytesseract.get_tesseract_version()
+    return pytesseract.image_to_string(Image.open('test.png'), lang='eng')
+     
+ def ocxz
+    pytesseract.pytesseract.tesseract_cmd = '/usr/local/bin/tesseract'
+    # test.png from the pytesseract project: https://github.com/madmaze/pytesseract/tree/master/tests/data
+    #return pytesseract.get_tesseract_version()
+    return pytesseract.image_to_string(Image.open('test.png'), lang='eng')
+        
 @app.errorhandler(404)
 def not_found(error):
   resp = jsonify( { 
