ed413df8a2e26c8f2de585f0aaedbd21eeb6879e
djkhz <zheezs@hotmail.com>
2023-09-19 00:11:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -53,62 +53,44 @@ Imageocr = Image.new(mode, size, color)
 Imnp =np.array([])
 # app.some_model = pd.read_excel("/app/data/sila_data.xlsx", sheet_name="Book lala", keep_default_na= False, na_values=[""])
 #pytesseract.pytesseract.tesseract_cmd = '/app/usr/bin/tesseract'
+
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
-
-@app.route('/test', methods=['GET','POST'])
-def test():
+    
+@app.route('/ocr', methods=['GET','POST'])
+def ocr():
     # test.png from the pytesseract project: https://github.com/madmaze/pytesseract/tree/master/tests/data
     #return pytesseract.get_tesseract_version()
-    return 'hot'
+    return pytesseract.image_to_string(Image.open('test.png'), lang='eng')
     
-# @app.route('/ocr', methods=['GET','POST'])
-# def ocr():
-#     # test.png from the pytesseract project: https://github.com/madmaze/pytesseract/tree/master/tests/data
-#     #return pytesseract.get_tesseract_version()
-#     return pytesseract.image_to_string(Image.open('test.png'), lang='eng')
-
-# @app.route('/ocx', methods=['GET','POST'])
-# def ocx():
-#     # test.png from the pytesseract project: https://github.com/madmaze/pytesseract/tree/master/tests/data
-#     #return pytesseract.get_tesseract_version()
-#     pytesseract.pytesseract.tesseract_cmd = '/app/.usr/bin/tesseract'
-#     return pytesseract.image_to_string(Image.open('test.png'), lang='eng')
-
-# @app.route('/ocy', methods=['GET','POST'])
-# def ocy():
-#     # test.png from the pytesseract project: https://github.com/madmaze/pytesseract/tree/master/tests/data
-#     #return pytesseract.get_tesseract_version()
-#     pytesseract.pytesseract.tesseract_cmd = '/app/usr/bin/tesseract'
-#     return pytesseract.image_to_string(Image.open('test.png'), lang='eng')
-
-# @app.route('/ocz', methods=['GET','POST'])
-# def ocz():
-#     # test.png from the pytesseract project: https://github.com/madmaze/pytesseract/tree/master/tests/data
-#     #return pytesseract.get_tesseract_version()
-#     pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
-#     return pytesseract.image_to_string(Image.open('test.png'), lang='eng')
-       
-# @app.errorhandler(404)
-# def not_found(error):
-#   resp = jsonify( { 
-#     u'status': 404, 
-#     u'message': u'Resource not found' 
-#   } )
-#   resp.status_code = 404
-#   return resp
+@app.route('/ocx', methods=['GET','POST'])
+def ocx():
+    pytesseract.pytesseract.tesseract_cmd = '/app/.usr/bin/tesseract'
+    # test.png from the pytesseract project: https://github.com/madmaze/pytesseract/tree/master/tests/data
+    #return pytesseract.get_tesseract_version()
+    return pytesseract.image_to_string(Image.open('test.png'), lang='eng')
     
-# def process_file(path, filename):
+@app.errorhandler(404)
+def not_found(error):
+  resp = jsonify( { 
+    u'status': 404, 
+    u'message': u'Resource not found' 
+  } )
+  resp.status_code = 404
+  return resp
     
-#     # json_data = {path: filename}
-#     # return json_data
-#     file_name = path.replace(filename,secure_filename(filename))
-#     # img3 = cv2.imread(file_name)
-#     # img3 = np.array(Imageocr)
-#     gray3 = cv2.cvtColor(Imnp, cv2.COLOR_BGR2GRAY) 
+def process_file(path, filename):
+    
+    # json_data = {path: filename}
+    # return json_data
+    file_name = path.replace(filename,secure_filename(filename))
+    # img3 = cv2.imread(file_name)
+    # img3 = np.array(Imageocr)
+    gray3 = cv2.cvtColor(Imnp, cv2.COLOR_BGR2GRAY) 
 
-#     text3 = pytesseract.image_to_string(gray3, lang='lao', config='--psm 11')
-#     # target = pytesseract.image_to_string(image, lang='eng', boxes=False, \
-#         # config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789')
-#     return text3
+    text3 = pytesseract.image_to_string(gray3, lang='lao', config='--psm 11')
+    # target = pytesseract.image_to_string(image, lang='eng', boxes=False, \
+        # config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789')
+    return text3
