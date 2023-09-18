4afcf45057f3d216e40a62c8abc69ca7f8c0ee0b
djkhz <zheezs@hotmail.com>
2023-09-18 13:47:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def hello_world():
 def ocr():
     # test.png from the pytesseract project: https://github.com/madmaze/pytesseract/tree/master/tests/data
     #return pytesseract.get_tesseract_version()
-    return pytesseract.image_to_string(Image.open('test.png'), lang='lao')
+    return pytesseract.image_to_string(Image.open('test.png'), lang='eng')
     
 @app.errorhandler(404)
 def not_found(error):
